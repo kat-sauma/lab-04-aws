7a18dd848d8724ccd9dc5d1286bf5b75b30f878b
Kat Sauma <katsauma@gmail.com>
2021-03-31 17:59:37 -0700

    initial push with readme and plans

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lab-04-aws
+
+1. Set up AWS account
+2. .env with keys and github secrets
+3. MODEL to upload a file 
+4. CRUD ROUTES (uploads.)
+    /GET 
+    upload a file /POST
+    /GET BY ID 
+    /PUT
+    /DELETE BY ID
---
+++ __tests__/app.test.js
@@ -0,0 +1,12 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('lab-04-aws routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('', )
+});
