99ed117a5bd4ad935acd8475f201e86988b82da0
Kat Sauma <katsauma@gmail.com>
2021-04-01 12:07:53 -0700

    test set up to write CRUD and trying to figure out first model for /POST

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,15 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-jest.mock('../lib/utils/aws');
-const s3Client = require('../lib/utils/aws');
+// jest.mock('../lib/utils/aws');
+// const s3Client = require('../lib/utils/aws');
 
-jest.mock('s3Client', () => () => ({
-  uploads: {
-    create: jest.fn(),
-  },
-}));
+// jest.mock('s3Client', () => () => ({
+//   uploads: {
+//     create: jest.fn(),
+//   },
+// }));
+
 
 describe('CRUD routes', () => {
   beforeEach(() => {
@@ -19,7 +20,21 @@ describe('CRUD routes', () => {
 
   let upload;
   beforeEach(async() => {
-    upload = await Upload.insert({ quantity: 10 })
+    upload = await Upload.insert({ 
+      fileName: 'Screen Shot 2020-08-27 at 2.13.30 PM',
+      fileBody: fileContent })
+    // s3Client.mockReset();
+  });
+
+  it('ASYNC/AWAIT: creates a new file to upload to our database', async () => {
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ file: 'Screen Shot 2020-08-27 at 2.13.30 PM' });
+
+      expect(res.body).toEqual({
+      id: '2',
+      file: 'Screen Shot 2020-08-27 at 2.13.30 PM',
+    });
   });
 
   it('', )
