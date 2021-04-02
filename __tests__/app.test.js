f50cd72a4e4be6f17616be3769a0737f16cd2cdf
Kat Sauma <katsauma@gmail.com>
2021-04-01 18:09:30 -0700

    wrote /POST method layered architecture and test written but running red. need to hit the bucket in aws

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,8 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Upload = require('../lib/models/Upload');
+
 
 // jest.mock('../lib/utils/aws');
 // const s3Client = require('../lib/utils/aws');
@@ -20,22 +22,19 @@ describe('CRUD routes', () => {
 
   let upload;
   beforeEach(async() => {
-    upload = await Upload.insert({ 
-      fileName: 'Screen Shot 2020-08-27 at 2.13.30 PM',
-      fileBody: fileContent })
+    upload = await Upload.insert({ fileName: 'Screen Shot 2020-08-27 at 2.33.36 PM.png' })
     // s3Client.mockReset();
   });
 
-  it('ASYNC/AWAIT: creates a new file to upload to our database', async () => {
+  it('creates a new file to upload to our database', async () => {
     const res = await request(app)
-      .post('/api/v1/orders')
-      .send({ file: 'Screen Shot 2020-08-27 at 2.13.30 PM' });
+      .post('/api/v1/uploads')
+      .send({ fileName: 'a-plant-photo.png' });
 
       expect(res.body).toEqual({
       id: '2',
-      file: 'Screen Shot 2020-08-27 at 2.13.30 PM',
+      filePath: 'https://crabbucket.s3-us-west-1.amazonaws.com/a-plant-photo.png',
     });
   });
 
-  it('', )
 });
